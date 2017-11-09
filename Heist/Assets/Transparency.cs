df39fd3c503b65e04201ab3df6a0bb2f7ac93480
grumbleBee <s.kyne96@gmail.com>
2017-11-09 11:32:22 -0500

    Transparency works (almost)!
    *siiiick guitar solo*

---
+++ Heist/Assets/Transparency.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparency : MonoBehaviour
+{
+    public float distance;
+
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, distance);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+
+            if (rend)
+            {
+                AutoTransparent AT = rend.GetComponent<AutoTransparent>();
+                if (AT == null)
+                {
+                    AT = rend.gameObject.AddComponent<AutoTransparent>();
+                }
+                AT.BeTransparent();
+            }
+        }
+    }
+}
